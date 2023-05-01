8bb422e9adcb52eb7a364f0304dddf69b6480454
Victor Muraki <japinnfs@gmail.com>
2023-05-01 09:04:40 -0300

    Add footer and subheader drafts

---
+++ src/components/SubHeader.vue
@@ -0,0 +1,27 @@
+<template>
+      <v-toolbar
+      density="compact"
+      color="primary"
+      dark
+      >
+      <v-toolbar-title>Deloitte Challenge</v-toolbar-title>
+        <v-spacer></v-spacer>
+  
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+  
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+  
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-toolbar>
+  </template>
+<script>
+  export default {
+    name: 'SubHeader'
+  }
+</script>
