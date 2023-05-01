c3f0ed4437a9dbd69a6b0d0d556039cfa9d0a56c
Victor Muraki <japinnfs@gmail.com>
2023-05-01 09:05:20 -0300

    Create cards view draft

---
+++ src/components/CardsView.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-card class="mx-auto">
+        <v-container fluid>
+            <v-row dense>
+                <v-col
+                    v-for="card in cards"
+                    :key="card.title"
+                    :cols="card.flex"
+                >
+                <CharacterCard
+                    :cards-data="card"
+                />
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-card>
+</template>
+  
+<script>
+import CharacterCard from '@/components/CharacterCard.vue';
+
+export default {
+    name: 'CardsView',
+    components: {
+        CharacterCard,
+    },
+    data: () => ({
+        cards: [
+            { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 6 },
+            { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+            { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+            { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+            { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 }
+        ],
+    }),
+}
+</script>
---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-card>
+        <v-img
+            :src="cardsData.src"
+            class="align-end"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            height="200px"
+            cover
+        />
+        <v-card-actions>
+            <span>{{ cardsData.title }}</span>
+            <v-spacer />
+            <v-btn icon color="surface-variant">
+                <v-icon>mdi-heart</v-icon>
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'CharacterCard',
+    props: {
+        cardsData: {
+            type: Object,
+            default: () => {}
+        },
+    },
+}
+</script>
